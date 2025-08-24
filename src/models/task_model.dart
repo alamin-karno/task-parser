83cace7fc6ff948105010df50d3aadcf92d6e520
Md. Al-Amin <amin15-1951@diu.edu.bd>
2025-08-24 23:15:30 +0600

    feat: added created and updated time attribute to Task model

--- src/models/task_model.dart
+++ src/models/task_model.dart
@@ -1,6 +1,12 @@
 class TaskModel {
-  TaskModel({required this.title, required this.description});
+  TaskModel({
+    required this.title,
+    required this.description,
+  })  : createdAt = DateTime.now(),
+        updatedAt = DateTime.now();
 
   final String title;
   final String description;
+  final DateTime createdAt;
+  final DateTime updatedAt;
 }
