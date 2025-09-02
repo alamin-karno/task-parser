cd3afbd990cd8bfb5468386a54b40cffd0cd6fd1
alamin-karno <alamin.karno@gmail.com>
2025-09-02 11:08:07 +0600

    fix: spelling mistake

--- src/main.dart
+++ src/main.dart
@@ -5,8 +5,8 @@ void main() async {
   final jsonInput = '''
 [
   {"title" : "Sample Programming task", "description" : "Create E-Commerce client", "platforms" : "WEB", "language" :"JavaScript", "type" : "Programming"},
-  {"title" : "Sample Math task", "description" : "Solve the claculus", "field" : "Calculus", "type" : "Mathematics"},
-  {"title" : "Sample Graphic Design task", "description" : "UX design for E-Comerce", "platforms" : "WEB", "output" : "Figma", "type" : "Graphics_Design"}
+  {"title" : "Sample Math task", "description" : "Solve the calculus", "field" : "Calculus", "type" : "Mathematics"},
+  {"title" : "Sample Graphic Design task", "description" : "UX design for E-commerce", "platforms" : "WEB", "output" : "Figma", "type" : "Graphics_Design"}
 ]
 ''';
 
